b856ccf517733f3fa08fd547ea6fa4b1e35bd704
tasklet1579 <tasklet1579@gmail.com>
2022-04-17 21:56:08 +0900

    feat(Ball): 투수가 선택한 숫자를 저장하는 래퍼 클래스

---
+++ src/main/java/baseball/dto/Ball.java
@@ -0,0 +1,13 @@
+package baseball.dto;
+
+public class Ball {
+    private int[] balls;
+
+    public Ball(int[] balls) {
+        this.balls = balls;
+    }
+
+    public int[] getBalls() {
+        return balls;
+    }
+}
