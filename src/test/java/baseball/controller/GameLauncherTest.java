717148d07f91cfea384f72a05e85b329403c5999
tasklet1579 <tasklet1579@gmail.com>
2022-04-18 00:39:49 +0900

    test(GameLauncherTest): 숫자 야구 게임 결과 테스트 - 게임 종료 테스트

--- src/test/java/baseball/controller/GameLauncherTest.java
+++ src/test/java/baseball/controller/GameLauncherTest.java
@@ -7,7 +7,11 @@ import baseball.model.Umpire;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
+import static baseball.controller.GameLauncher.isPlayerVictory;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class GameLauncherTest {
     @DisplayName("ONE 볼 카운팅 테스트")
@@ -64,7 +68,7 @@ class GameLauncherTest {
         // given
         String expected = "낫싱";
 
-        //
+        // when
         int[] pitcher = {0, 0, 0, 3, 0, 0, 2, 0, 0, 1};
         char[] player = {'1', '2', '8'};
         Record record = Umpire.getInstance().createMatchRecord(new Match(pitcher, player));
@@ -72,5 +76,29 @@ class GameLauncherTest {
 
         // then
         assertThat(result.getMessage()).contains(expected);
+    }
+
+    @DisplayName("게임 종료 테스트")
+    @Test
+    void 게임_종료_테스트() {
+        // given
+        String expected = "3스트라이크\n"
+                + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+        // when
+        int[] pitcher = {0, 0, 1, 2, 3, 0, 0, 0, 0, 0};
+        char[] player = {'2', '3', '4'};
+        Record record = Umpire.getInstance().createMatchRecord(new Match(pitcher, player));
+        String actual = new Message(record.getStrike(), record.getBall()).getMessage();
+
+        // then
+        assertThat(actual).contains(expected);
+
+        // then
+        assertThatCode(() -> {
+            System.setIn(new ByteArrayInputStream("2".getBytes()));
+            isPlayerVictory(new Message(record.getStrike(), record.getBall()));
+        }).doesNotThrowAnyException();
     }
 }
